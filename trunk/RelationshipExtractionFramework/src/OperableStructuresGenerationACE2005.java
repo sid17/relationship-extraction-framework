f033b6422d1d0f946d9620c2b52a4803ad262ed4
gonfsimoes@gmail.com <gonfsimoes@gmail.com@e12225c4-5e20-853a-98d5-482a265fe6a0>
2011-09-15 04:47:42 +0000

    My version of JSRE working

--- trunk/RelationshipExtractionFramework/src/OperableStructuresGenerationACE2005.java
+++ trunk/RelationshipExtractionFramework/src/OperableStructuresGenerationACE2005.java
@@ -9,6 +9,7 @@ import edu.columbia.cs.cg.document.Document;
 import edu.columbia.cs.og.algorithm.StructureGenerator;
 import edu.columbia.cs.og.configuration.StructureConfiguration;
 import edu.columbia.cs.og.core.CoreWriter;
+import edu.columbia.cs.og.core.impl.BagOfNGramsKernel;
 import edu.columbia.cs.og.core.impl.SubsequencesKernel;
 import edu.columbia.cs.og.structure.OperableStructure;
 import edu.columbia.cs.utils.SGMFileFilter;
@@ -18,11 +19,11 @@ import edu.columbia.cs.utils.SGMFileFilter;
 public class OperableStructuresGenerationACE2005 {
 
 	public static void main(String[] args) throws IOException, ClassNotFoundException {
-		StructureConfiguration conf = new StructureConfiguration(new SubsequencesKernel());
+		StructureConfiguration conf = new StructureConfiguration(new BagOfNGramsKernel());
 		String inputFolder = "/home/goncalo/ACEProcessedFlat/";
 		File ACEDir = new File(inputFolder);
 		File[] files = ACEDir.listFiles();
-		String outputFolder = "/home/goncalo/ACESubseqFlat/";
+		String outputFolder = "/home/goncalo/ACEBoNGramsFlat/";
 		for(int i=0; i<files.length; i++){
 			System.out.println("Processing file " + (i+1) + " of " + files.length);
 			File f = files[i];
